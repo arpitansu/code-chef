91314957efb9db12175fa6388ac0f6d532e0794b
arpitansu <arpit.pandey05@gmail.com>
2016-10-13 20:34:50 +0530

    fix

--- begginer/ChefAndInequality.py
+++ begginer/ChefAndInequality.py
@@ -2,9 +2,6 @@ t = input()
 for i in xrange(t):
 	n = 0
 	k = 0
-	# ar = raw_input().split(" ")
-	# ar = [int(x) for x in ar]
-	# a, b, c, d = ar[0], ar[1], ar[2], ar[3]
 	a, b, c, d = map(int, raw_input().split())
 	if c <= a:
 		c = a + 1
@@ -16,5 +13,5 @@ for i in xrange(t):
 		c = b + 1
 	if b < a or d < c or d < a:
 		print 0
-	# else:
+	# else: if does'nt works :-  use else and put print statment below inside else 
 	print (d - c + 1) * (b - a + 1) + (n * (2 * k + n - 1) /2) 

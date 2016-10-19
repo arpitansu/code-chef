7532aeb9c07260f489b7a3530c91861b0559d48e
arpitansu <arpit.pandey05@gmail.com>
2016-10-19 19:17:55 +0530

    initial

---
+++ easy/subtractionGame1.py
@@ -0,0 +1,17 @@
+# this file has two algo's 
+
+
+# this is pure logic
+for _ in xrange(input()):# testcases
+	numberOfNumbers = input() #fooling around
+	numList = map(int, raw_input().split()) # list for numbers or arrays
+	while min(numList) != max(numList):# checks if the max value in list in equal to min value or not			
+		numList.insert(0, numList.pop(numList.index(max(numList))) - min(numList))# maxIs = max(numList)# minIs = min(numList)
+	print numList[0]
+
+#this is pure smartness
+from fractions import gcd
+for _ in xrange(input()):
+	numberOfNumbers = input()
+	numList = map(int, raw_input().split())
+	print reduce(gcd,numList) 
